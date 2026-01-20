195efd5177f62db5ad55196a6ad1774da6fb8419
Kuroro Lucifer <jolann.madec21@gmail.com>
2026-01-20 16:31:15 +0100

    Update script.user.js

--- public/script.user.js
+++ public/script.user.js
@@ -31,8 +31,8 @@ const BASE_URL = "https://grepolis-temple-notifier-six.vercel.app";
 const settings = {
     send_support_message: true,
     send_attack_message: true,
-    discord_support_hook: "https://discord.com/api/webhooks/1463190102686695425/gWvOqP_rmcMQt-pMBkJuuuqF_NylOVWwDKASyTUu4hvuLeOLHirFUemH_kKRL42G55gV",
-    discord_attack_hook: "https://discord.com/api/webhooks/1463164065592643704/t6PJdAfoddha7MH6NVXoBF8FTk62vMWPXklb4mEyjJ9VRr1n9L8IwbSQ3fZSxPbwsPXc",
+    discord_support_hook: "https://discord.com/api/webhooks/1463164065592643704/t6PJdAfoddha7MH6NVXoBF8FTk62vMWPXklb4mEyjJ9VRr1n9L8IwbSQ3fZSxPbwsPXc",
+    discord_attack_hook: "https://discord.com/api/webhooks/1463190102686695425/gWvOqP_rmcMQt-pMBkJuuuqF_NylOVWwDKASyTUu4hvuLeOLHirFUemH_kKRL42G55gV",
     monitor_timeout: 60000,
 };
 
@@ -105,7 +105,7 @@ function loadSettings() {
     settings.send_attack_message = GM_getValue("setting_send_attack_message", true);
     settings.discord_support_hook = GM_getValue("setting_discord_support_hook", "[Discord Webhook URL here]");
     settings.discord_attack_hook = GM_getValue("setting_discord_attack_hook", "[Discord Webhook URL here]");
-    settings.monitor_timeout = GM_getValue("setting_monitor_timeout", 60000);
+    settings.monitor_timeout = GM_getValue("setting_monitor_timeout", 10000);
 }
 
 async function monitor() {
